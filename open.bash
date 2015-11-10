91dcb5d024068d9ff21a8e4bb20e0085abbde54d
Spencer Buyansky <sbuyansky@gmail.com>
2015-11-10 12:23:42 -0500

    add files

---
+++ open.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+vim -p app.js public/javascripts/app.js public/javascripts/controllers/quiz.js routes/api.js routes/index.js views/index.html views/templates/quiz.html public/stylesheets/style.css views/templates/quiz_create.html
